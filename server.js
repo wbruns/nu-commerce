0855872b5c3c26258cc6a39ed41780437fed5934
wbruns <wbruns07@gmail.com>
2022-04-09 13:59:59 -0500

    added .env and dotenv connection, database connection and sequelize session connection

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: process.env.APP_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
